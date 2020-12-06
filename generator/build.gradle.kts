4204e60068fefbdeeb8e6f7c00fef2511c63c8f6
Tom Koptel <tom.koptel@gmail.com>
2020-12-06 14:36:33 +0100

    Add Jacoco

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,7 +1,33 @@
 plugins {
     kotlin("jvm")
+    jacoco
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.jacocoTestReport)
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "0".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
