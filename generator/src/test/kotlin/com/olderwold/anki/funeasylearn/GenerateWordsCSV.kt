a3363486f6f2c3fedf91dcfd8f0027621add477c
Tom Koptel <tom.koptel@gmail.com>
2021-07-03 19:12:15 +0200

    Generate HU words from 541 to 560

--- generator/src/test/kotlin/com/olderwold/anki/funeasylearn/GenerateWordsCSV.kt
+++ generator/src/test/kotlin/com/olderwold/anki/funeasylearn/GenerateWordsCSV.kt
@@ -195,6 +195,12 @@ class GenerateWordsCSV {
 
     @Test
     @OkReplay
+    fun words_hu_541_560() {
+        generateWords(start = 541, end = 560, language = Language.HU)
+    }
+
+    @Test
+    @OkReplay
     fun words_pl_1_100() {
         generateWords(start = 1, end = 100, language = Language.PL)
     }
